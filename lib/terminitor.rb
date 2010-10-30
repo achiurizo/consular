86ef062381114cb3b34c1dacfedbae0f442f1552
Arthur Chiu <Mr.Arthur.Chiu@gmail.com>
2010-10-29 18:58:04 -0700

    minor path clean up

--- lib/terminitor.rb
+++ lib/terminitor.rb
@@ -1,21 +1,20 @@
-require 'rubygems'
-require File.expand_path('../terminitor/yaml', __FILE__)
-require File.expand_path('../terminitor/dsl', __FILE__)
-require File.expand_path('../terminitor/runner', __FILE__)
-require File.expand_path('../terminitor/abstract_core', __FILE__)
-require File.expand_path('../terminitor/cli', __FILE__)
-require File.expand_path('../terminitor/abstract_capture', __FILE__)
+require 'terminitor/yaml'
+require 'terminitor/dsl'
+require 'terminitor/runner'
+require 'terminitor/abstract_core'
+require 'terminitor/cli'
+require 'terminitor/abstract_capture'
 
 module Terminitor
-  autoload :Version, File.expand_path('../terminitor/version', __FILE__)
+  autoload :Version, 'terminitor/version'
   case RUBY_PLATFORM.downcase
   when %r{darwin}
     require 'appscript'
-    autoload :MacCore,     File.expand_path('../terminitor/cores/mac_core', __FILE__)
-    autoload :MacCapture,  File.expand_path('../terminitor/capture/mac_capture', __FILE__)    
+    autoload :MacCore,        'terminitor/cores/mac_core'
+    autoload :MacCapture,     'terminitor/capture/mac_capture'  
   when %r{linux}
     require 'dbus'
-    autoload :KonsoleCore, File.expand_path('../terminitor/cores/konsole_core', __FILE__)
-    autoload :KonsoleCapture,  File.expand_path('../terminitor/capture/konsole_capture', __FILE__)        
+    autoload :KonsoleCore,    'terminitor/cores/konsole_core'
+    autoload :KonsoleCapture, 'terminitor/capture/konsole_capture'    
   end
 end
