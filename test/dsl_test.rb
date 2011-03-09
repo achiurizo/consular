3e197ca80afa8d4f2010dc3003faf490403d5bbb
Arthur Chiu <Mr.Arthur.Chiu@gmail.com>
2011-03-09 10:04:21 -0800

    [dsl] update tests to check for parenthesized args

--- test/dsl_test.rb
+++ test/dsl_test.rb
@@ -1,35 +1,89 @@
 require File.expand_path('../teststrap', __FILE__)
 
 context "Dsl" do
-  setup { @path = File.expand_path('../fixtures/bar.term', __FILE__)}
-  setup { @yaml = Terminitor::Dsl.new(@path) }
+  setup { Terminitor::Dsl.new File.expand_path('../fixtures/bar.term', __FILE__) }
   asserts_topic.assigns :setup
   asserts_topic.assigns :windows
   asserts_topic.assigns :_context
  
   context "to_hash" do
-    setup { @yaml.to_hash }
-    asserts_topic.equivalent_to :setup=>["echo \"setup\""], 
-                                :windows=>{
-                                  "window1"=>{:tabs=>{"tab2"=>{:commands=>["echo 'named tab'", "ls"], 
-                                                               :options => {:name => "named tab", :settings=>"Grass"}}, 
-                                                      "tab1"=>{:commands=>["echo 'first tab'", "echo 'of window'", "echo 'than now'"]},
-                                                      "tab3"=>{:commands=>["top"],
-                                                               :options =>{:name => "a tab", :settings => "Pro"}},
-                                                      "tab4"=>{:commands=>["ls"],
-                                                               :options =>{:name => "another named tab", :settings => "Grass"}},
-                                                      "default"=>{:commands=>['whoami && who && ls']}
-                                                     },
-                                              :before => ['cd /path'],
-                                              :options => {:size=>[70,30]}},
-                                  "window2"=>{:tabs=>{"tab1"=>{:commands=>["uptime"]},
-                                                      "default"=>{:commands=>[]}
-                                                     },
-                                              :before => ['whoami'],
-                                              :options => {:name => 'server'}},
-                                  "default"=>{:tabs=>{"tab1"=>{:commands=>["echo 'default'", "echo 'default tab'", "ok", "for real"]},
-                                                      "default"=>{:commands=>[]}
-                                                     }}}
-  end
+    setup { topic.to_hash }
+    asserts([:[],:setup]).equals ["echo \"setup\""]
+    context ":windows" do
+      setup { topic[:windows] }
 
+      context "with 'window1' key" do
+        setup { topic['window1'] }
+
+        asserts([:[],:before]).equals ['cd /path']
+        asserts([:[],:options]).equals({ :size => [70,30]})
+
+        context "with :tabs" do
+          setup { topic[:tabs] }
+          
+          asserts([:[], 'tab2']).equivalent_to({
+            :commands=>["(echo 'named tab')", "(ls)"], 
+            :options => {
+              :name => "named tab",
+              :settings=>"Grass"
+            }
+          })
+
+          asserts([:[], 'tab1']).equivalent_to({
+            :commands=>["echo 'first tab'", "echo 'of window'", "echo 'than now'"]
+          })
+
+          asserts([:[],'tab3']).equivalent_to({
+            :commands=>["(top)"],
+            :options =>{
+              :name => "a tab",
+              :settings => "Pro"
+            }
+          })
+
+          asserts([:[],'tab4']).equivalent_to({
+            :commands=>["(ls)"],
+            :options =>{
+              :name => "another named tab",
+              :settings => "Grass"
+            }
+          })
+
+          asserts([:[],'default']).equivalent_to({
+            :commands=>['(whoami) && (who) && (ls)']
+          })
+        end
+
+      end
+
+      context "with 'window2' key" do
+        setup { topic['window2'] }
+
+        asserts([:[],:before]).equals ['(whoami)']
+
+        context "with :tabs" do
+          setup { topic[:tabs] }
+
+          asserts([:[], 'tab1']).equals({ :commands => ["uptime"]})
+          asserts([:[], 'default']).equals({ :commands => []})
+        end
+      end
+
+      context "with 'default' key" do
+        setup { topic['default'] }
+
+        context "with :tags key" do
+          setup { topic[:tabs] }
+
+          asserts([:[],'tab1']).equals({
+            :commands=>["echo 'default'", "echo 'default tab'", "ok", "for real"]
+          })
+
+          asserts([:[],'default']).equals({
+            :commands => []
+          })
+        end
+      end
+    end
+  end
 end
